bfe48113fa90f7ad38c1c299edf14622409a8432
Kiarash Shojaei Arani <kiarash.sh1383@gmail.com>
2024-05-15 23:34:33 +0330

    android creating canceled

--- desktop/src/com/ap/bendandconquer/DesktopLauncher.java
+++ desktop/src/com/ap/bendandconquer/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("BendAndConquer");
+		config.setTitle("Bend And Conquer");
 		new Lwjgl3Application(new BendAndConquer(), config);
 	}
 }
