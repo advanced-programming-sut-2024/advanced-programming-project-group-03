bfd9237759228833690226adbcdd6973546e3883
nima1399 <hekmatinima@gmail.com>
2024-05-27 23:14:46 +0330

    added Leader.java

---
+++ core/src/ir/ap/probending/Model/Leader.java
@@ -0,0 +1,21 @@
+package ir.ap.probending.Model;
+
+public class Leader {
+//    private Faction faction;
+    private String name;
+    // private LeaderAbility leaderAbility;
+
+    public Leader(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
