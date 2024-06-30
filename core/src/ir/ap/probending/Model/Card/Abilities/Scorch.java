dfcb8a19449155749aae99cb7b6c9c97f4f5de4a
Kiarash Shojaei Arani <kiarash.sh1383@gmail.com>
2024-06-30 12:08:29 +0330

    spy added and card list window added and scorch added

--- core/src/ir/ap/probending/Model/Card/Abilities/Scorch.java
+++ core/src/ir/ap/probending/Model/Card/Abilities/Scorch.java
@@ -1,9 +1,45 @@
 package ir.ap.probending.Model.Card.Abilities;
 
 import ir.ap.probending.Model.Card.Card;
+import ir.ap.probending.Model.Game.Game;
+
+import java.util.ArrayList;
 
 public class Scorch extends Ability{
     public void executeAbility(Card card) {
-        // TODO implement here
+        int highestPower = 0;
+
+        ArrayList<Card> cards1 = new ArrayList<>(Game.getGame().getGameBoard().getPlayer1Board().getAllCards());
+        ArrayList<Card> cards2 = new ArrayList<>(Game.getGame().getGameBoard().getPlayer2Board().getAllCards());
+
+        for (Card c : cards1) {
+            if (c.getPower() > highestPower && !c.isHero() && !c.equals(card)) {
+                highestPower = c.getPower();
+            }
+        }
+
+        for (Card c : cards2) {
+            if (c.getPower() > highestPower && !c.isHero() && !c.equals(card)){
+                highestPower = c.getPower();
+            }
+        }
+
+        if (highestPower > 0) {
+            for (Card c : cards1) {
+                if (c.getPower() == highestPower && !c.isHero() && !c.equals(card)) {
+                    Game.getGame().getGameBoard().getPlayer1Board().removeCardFromCloseCombat(c);
+                    Game.getGame().getGameBoard().getPlayer1Board().removeCardFromRanged(c);
+                    Game.getGame().getGameBoard().getPlayer1Board().removeCardFromSiege(c);
+                }
+            }
+
+            for (Card c : cards2) {
+                if (c.getPower() == highestPower && !c.isHero() && !c.equals(card)) {
+                    Game.getGame().getGameBoard().getPlayer2Board().removeCardFromCloseCombat(c);
+                    Game.getGame().getGameBoard().getPlayer2Board().removeCardFromRanged(c);
+                    Game.getGame().getGameBoard().getPlayer2Board().removeCardFromSiege(c);
+                }
+            }
+        }
     }
 }
