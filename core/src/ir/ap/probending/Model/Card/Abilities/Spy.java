b5cd475b3dc8c1175664810cb7b5ae4fa1542000
Kiarash Shojaei Arani <kiarash.sh1383@gmail.com>
2024-06-30 15:32:00 +0330

    spy fixed and medic ability added

--- core/src/ir/ap/probending/Model/Card/Abilities/Spy.java
+++ core/src/ir/ap/probending/Model/Card/Abilities/Spy.java
@@ -6,6 +6,33 @@ import ir.ap.probending.Model.Game.GameBoard;
 
 public class Spy extends Ability {
     public void executeAbility(Card card) {
+        GameBoard gameBoard = Game.getGame().getGameBoard();
+        if (gameBoard.getPlayer1Board().getCloseCombat().contains(card)) {
+            gameBoard.getPlayer2Board().addCardToCloseCombat(card);
+            gameBoard.getPlayer1Board().removeCardFromCloseCombat(card);
+        }
+        else if (gameBoard.getPlayer1Board().getRanged().contains(card)) {
+            gameBoard.getPlayer2Board().addCardToRanged(card);
+            gameBoard.getPlayer1Board().removeCardFromRanged(card);
+        }
+        else if (gameBoard.getPlayer1Board().getSiege().contains(card)) {
+            gameBoard.getPlayer2Board().addCardToSiege(card);
+            gameBoard.getPlayer1Board().removeCardFromSiege(card);
+        }
+        else if (gameBoard.getPlayer2Board().getCloseCombat().contains(card)) {
+            gameBoard.getPlayer1Board().addCardToCloseCombat(card);
+            gameBoard.getPlayer2Board().removeCardFromCloseCombat(card);
+        }
+        else if (gameBoard.getPlayer2Board().getRanged().contains(card)) {
+            gameBoard.getPlayer1Board().addCardToRanged(card);
+            gameBoard.getPlayer2Board().removeCardFromRanged(card);
+        }
+        else if (gameBoard.getPlayer2Board().getSiege().contains(card)) {
+            gameBoard.getPlayer1Board().addCardToSiege(card);
+            gameBoard.getPlayer2Board().removeCardFromSiege(card);
+        }
 
+        Game.getGame().getCurrentPlayer().drawCard();
+        Game.getGame().getCurrentPlayer().drawCard();
     }
 }
